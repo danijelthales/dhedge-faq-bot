d1bc5f7b27d7e4d8b577101f490b1c2f573b7d27
dgornjakovic <danijel.gornjakovic@prodyna.com>
2020-09-08 23:13:14 +0200

    dhedge faq

--- faq.js
+++ faq.js
@@ -682,28 +682,6 @@ client.on("message", msg => {
                         console.log("Error: " + err.message);
                     });
 
-                } else if (command == "13") {
-
-                    var today = new Date();
-                    while (today > payday) {
-                        payday.setDate(payday.getDate() + 7);
-                    }
-                    var difference = payday.getTime() - today.getTime();
-                    var seconds = Math.floor(difference / 1000);
-                    var minutes = Math.floor(seconds / 60);
-                    var hours = Math.floor(minutes / 60);
-                    var days = Math.floor(hours / 24);
-                    hours %= 24;
-                    minutes %= 60;
-                    seconds %= 60;
-
-                    exampleEmbed.addField("Countdown:", days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ", false);
-                    if (doReply) {
-                        msg.reply(exampleEmbed);
-                    } else {
-                        msg.channel.send(exampleEmbed);
-                    }
-
                 } else if (command == "62") {
 
                     exampleEmbed.addField("Volume in this period:", periodVolume, false);
